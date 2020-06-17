61c169a03d181839c8b66900331f11639005c20d
Abhinav <abhinuclear01@gmail.com>
2020-06-16 20:24:04 -0400

    All_Track

---
+++ Step 2/unit_6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is Unit 6 of the SPRINGBOARD DATA SCIENCE CAREER TRACK',
+    author='Abhinav Gairola',
+    license='',
+)
